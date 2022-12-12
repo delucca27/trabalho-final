2908e1b35c447c97c92b87744c016a511dc32c77
delucca27 <dev.pedrolucca@gmail.com>
2022-12-11 22:57:07 -0300

    Configuração do Router (Bruto)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
+// components and pages
+import Navbar from './components/Navbar.component'
 import SearchPage from './pages/Search.page'
+import HomePage from './pages/Home.page'
 
 export default function App() {
   return (
-    <>
-      <SearchPage />
-    </>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element = { <HomePage /> } />
+          <Route path="/search" element = { <SearchPage /> } />
+        </Routes>
+
+
+      </div>
+    </Router>
   )
 }
