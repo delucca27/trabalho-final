136be961a3ebe495ae19c99e3e981fe2f39c155f
delucca27 <dev.pedrolucca@gmail.com>
2022-12-13 10:15:10 -0300

    Mais estilização

--- src/pages/NotFound.page.jsx
+++ src/pages/NotFound.page.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function NotFoundPage() {
    return (
+   <>
    <div className="not-found">
-      <h1>Oops!</h1>
+      <h1>Oops!</h1> <br />
+      <h2>Essa página ainda não existe</h2>
+      <button className="cta-normal">Take me hoooooome ♫</button>
    </div>
+   </>
    )
 }
